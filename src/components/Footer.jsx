0e26e652c8740ef3a96bad020418093f78fc87e7
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-05-28 19:58:27 +1200

    add social media icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from "react"
+import { 
+  faLinkedin,
+  faFacebook,
+  faInstagram,
+  faGithub
+ } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
   return (
-    <footer></footer>
+    <footer>
+      <FontAwesomeIcon icon={faLinkedin} />
+      <FontAwesomeIcon icon={faFacebook} />
+      <FontAwesomeIcon icon={faInstagram} />
+      <FontAwesomeIcon icon={faGithub} />
+    </footer>
   )
 }
-
 
 // icons to add:
 // facebook / instagram / linkedin / github
---
+++ src/fontawesome.js
@@ -0,0 +1,16 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { 
+  faLinkedin,
+  faFacebook,
+  faInstagram,
+  faGithub
+ } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  faLinkedin,
+  faFacebook,
+  faInstagram,
+  faGithub
+  // more icons go here
+);
