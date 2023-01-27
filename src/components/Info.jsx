d9003521c74ff9c35150c7ebec5b04d6d9dbc6b2
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-01-27 14:07:48 +1300

    created two buttons button for email with mailto functionality button for phone with tel functionality added an icon for both buttons

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,6 +4,7 @@ import About from './About'
 import Interests from './Interests'
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function Info() {
   let url ="https://beforeforty.hashnode.dev/"
@@ -15,10 +16,18 @@ export default function Info() {
         <h1 className="details--name">Claudette Cabahug</h1>
         <h3 className="details--job_title">Fullstack Web Developer</h3>
         <a className="url--blogsite" href={url}>beforeforty.hashnode.dev</a>
-        <button className="email--text">
-          <FontAwesomeIcon icon={faEnvelope} size="lg"/>
-          Email
-        </button>
+        <div className="button-div">
+          <button className="email--text">
+              <a href="mailto:claudette.cabahug@outlook.com?subject=Hello">
+                <FontAwesomeIcon icon={faEnvelope} size="lg"/>
+              </a>
+          </button>
+          <button>
+              <a href="tel:+64211443593">
+                <FontAwesomeIcon icon={faPhone} size="lg"/>
+              </a>
+          </button>
+        </div>
         <div className="details--inner_container">
           <About />
           <Interests />
